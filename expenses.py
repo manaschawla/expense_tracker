af8ccaf2d8bb0e5a6bf8a6f7553f02a8d185588c
manaschawla <manaschawla324@gmail.com>
2024-08-02 13:32:05 +0530

    added new functins and enhanced the code

--- expenses.py
+++ expenses.py
@@ -1,8 +1,19 @@
-def add_expenses():
-    pass
-
-def view_expenses():
-    pass
+import json
+filename = 'data.txt'
+def initialize_file():
+    with open(filename, 'r') as file:
+        file.read()    
+        with open(filename, 'w') as file:
+            file.write("Date,Category,Amount\n")
+def add_expenses(date, category, amount):
+    with open(filename, 'a') as file:
+        file.write(f"{date}, {category}, {amount}\n")
+    
+def view_expenses(search):
+    with open(filename,'r') as file:
+        view = json.loads(filename)
+        print(view[search])
+        
 
 def total_expenses():
     pass
@@ -14,16 +25,15 @@ while True:
                                     1.Add Expenses with details
                                     2.View expenses by category
                                     3.Display the total expenses""")
-    while True:
-            try:
-                choice = int(input("enter your choice via 1/2/3: "))
-                break
-            except ValueError:
-                print("PLEASE GIVE THE VALUE IN INTEGER FORM ONLY AND UPTO 3. ")
+    choice = input("enter your choice via 1/2/3: ")
     if choice == '1':
-        pass
+        date = input("enter the date(dd/mm/yy): ")
+        amount = float(input("enter the amount you spend: "))
+        category = input("enter the category you spend the money: ")
+        add_expenses(date, category, amount)
     elif choice == '2':
-        pass
+        search_category = input("enter the category from which you want to see the expenses: ")
+        view_expenses(search_category)
     elif choice == '3':
         pass
     choice2 = input("""Do You Want To Perform More Operations
